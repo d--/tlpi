5de5d45e326b4c8a4cf307391f253d7541628a0b
d-- <d--@users.noreply.github.com>
2015-11-05 15:50:44 +0000

    added getopt sample code

--- dtee/dtee.c
+++ dtee/dtee.c
@@ -1,7 +1,20 @@
 #include <sys/types.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    return 0;
+    int option;
+    while ((option = getopt(argc, argv, "b")) != -1) {
+        switch (option) {
+        case 'b':
+            printf("butt\n");
+            break;
+        default:
+            exit(EXIT_FAILURE);
+        }
+    }
+    exit(EXIT_SUCCESS);
 }
