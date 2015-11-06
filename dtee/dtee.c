6b580e6ca5e8b576494c98dbb1f10c76dec94ee0
d-- <d--@users.noreply.github.com>
2015-11-06 00:23:45 +0000

    initial file and file append functionality working

--- dtee/dtee.c
+++ dtee/dtee.c
@@ -1,29 +1,47 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <getopt.h>
 
-#define DEBUG(...) fprintf(stderr, "DEBUG: "); fprintf(stderr, __VA_ARGS__);
+#define DEBUG(...) { \
+    fprintf(stderr,"DEBUG: "); \
+    fprintf(stderr,__VA_ARGS__); \
+}
+#define ASSERT(cond, err) { \
+    if(cond) { \
+        fprintf(stderr, err); \
+        exit(EXIT_FAILURE); \
+    } \
+}
 
 int main(int argc, char *argv[])
 {
     // get options
     int option;
+    int append = 0;
     while ((option = getopt(argc, argv, "a")) != -1) {
         switch (option) {
         case 'a':
-            DEBUG("append mode not currently implemented\n");
+            append = 1;
             break;
         default:
             exit(EXIT_FAILURE);
         }
     }
-    if (optind < argc) {
-        DEBUG("filename argument not currently implemented\n");
+
+    // get other args
+    int have_more_args = (optind < argc);
+    int fout = 0;
+    char *fname = NULL;
+    if (have_more_args) {
+        fout = 1;
+        fname = argv[optind];
     }
+    ASSERT(fname == NULL && append, "-a: Specify a filename.\n");
 
     // create a buffer that is the appropriate size 
     struct stat bufstat;
@@ -33,22 +51,38 @@ int main(int argc, char *argv[])
     }
     blksize_t bsize = bufstat.st_blksize;
     static char *buffer;
-    if ((buffer = malloc(bsize)) == NULL) {
-        fprintf(stderr, "error allocating memory\n");
-        exit(EXIT_FAILURE);
-    }
+    ASSERT((buffer = malloc(bsize)) == NULL, "error allocating memory\n");
     memset(buffer, 0, bsize);
 
-    // write directly from stdin to stdout
-    int st;
-    while ((st = read(STDIN_FILENO, buffer, bsize)) > 0) {
-        if (write(STDOUT_FILENO, buffer, bsize) == -1) {
-           perror("write");
-           exit(EXIT_FAILURE);
+    // open file
+    int fd;
+    if (fout) {
+        int flags = O_WRONLY | O_CREAT | O_TRUNC;
+        if (append) {
+            flags |= O_APPEND;
+            flags ^= O_TRUNC;
+        }
+        fd = open(fname, flags, 0644); 
+        if (fd == -1) {
+            perror("open");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // write to stdout and file
+    int readlen;
+    while ((readlen = read(STDIN_FILENO, buffer, bsize)) > 0) {
+        if (write(STDOUT_FILENO, buffer, readlen) == -1) {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
+        if (fout && write(fd, buffer, readlen) == -1) {
+            perror("file write");
+            exit(EXIT_FAILURE);
         }
         memset(buffer, 0, bsize);
     }
-    if (st == -1) {
+    if (readlen == -1) {
         perror("read");
         exit(EXIT_FAILURE);
     }
