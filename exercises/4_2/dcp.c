919fdeec8033d8eb19c5888a132e075b9bbee2d5
d-- <d--@users.noreply.github.com>
2015-11-07 02:48:29 +0000

    added .gitignore, initial src / dest file open code

--- exercises/4_2/dcp.c
+++ exercises/4_2/dcp.c
@@ -1,4 +1,31 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int main(int argc, char *argv[])
 {
+    if (argc < 2) {
+        fprintf(stderr, "Please specify a target file.\n");
+        exit(EXIT_FAILURE);
+    }
+    int src_fd, src_flags;
+    src_flags = O_RDONLY;
+    if ((src_fd = open(argv[1], src_flags)) == -1) {
+        perror("source file");
+        exit(EXIT_FAILURE);
+    }
+
+    if (argc < 3) {
+        fprintf(stderr, "Please specify a destination file.\n");
+        exit(EXIT_FAILURE);
+    }
+    int dest_fd, dest_flags;
+    dest_flags = O_WRONLY | O_CREAT | O_TRUNC;
+    if ((dest_fd = open(argv[2], dest_flags, 0644)) == -1) {
+        perror("destination file");
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
